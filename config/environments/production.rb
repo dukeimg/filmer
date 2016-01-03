ebc260b5ce43d6b2db5cc8a15596f86f29fd28d9
dukeimg <duke.img@gmail.com>
2016-01-03 19:53:56 +0300

    Heroku Mailer Fix v2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Mailer configuration
+  config.action_mailer.default_url_options = { host: 'https://filmer-dev.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
