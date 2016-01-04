2590144de0ceed3c7281d5bf937ebcb2261f259e
dukeimg <duke.img@gmail.com>
2016-01-04 13:26:58 +0300

    Multiple Devise changes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -14,6 +14,10 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def after_sign_up_path_for(resource)
-    root_path
+    new_user_session_path
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+    new_user_session_path
   end
 end
