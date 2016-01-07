d81e6cf2cb929697903f8dccc68c4ca0f1efb5a3
Viktor <ueC0laemorohsogh>
2016-01-07 16:49:48 +0300

    Documentation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Reconfigure below to use in production
+
   # Mailer configuration
   config.action_mailer.default_url_options = { host: 'https://filmer-dev.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
